68aceb67a9b77edf88b436f18ee3da0bf9337c57
rdufausansot <rachel.dufau@outlook.fr>
2023-04-05 11:48:10 +0200

    rating

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request, url_for, redirect
+import json
 from pymongo import MongoClient
+from bson import json_util
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
 
 db = client.flask_db
-todos = db.todos
+rating = db.rating
+
+
 
 # @app.route("/")
 # def hello_world():
@@ -15,13 +19,30 @@ todos = db.todos
 # def index():
 #     return render_template('index.html')
 
-@app.route('/', methods=('GET', 'POST'))
+@app.route('/', methods=('POST'))
 def index():
     if request.method=='POST':
-        content = request.form['content']
-        degree = request.form['degree']
-        todos.insert_one({'content': content, 'degree': degree})
-        return redirect(url_for('index'))
+        user = request.form['user_id']
+        anime = request.form['anime_id']
+        rating = request.form['rating']
 
-    all_todos = todos.find()
-    return render_template('index.html', todos=all_todos)
+        #content = request.form['content']
+        #myquery = { "content": content }
+        #degree = request.form['degree']
+        #newvalues = { "$set": { "degree": degree } }
+
+        """ mydoc = todos.find_one(myquery)
+        print("mydoc: ", mydoc)
+        if (mydoc == None):
+            todos.insert_one({'content': content, 'degree': degree})
+        else:
+            todos.update_one(myquery, newvalues) """
+        mydoc = {'user_id': user, 'anime_id': anime, 'rating': rating}
+        todos.insert_one(mydoc)
+        return json.loads(json_util.dumps(mydoc))
+    #return "rqting creqted"
+
+@app.route('/get-rating', methods=('GET'))
+def get_rating():
+    if request.method=='GET':
+        
